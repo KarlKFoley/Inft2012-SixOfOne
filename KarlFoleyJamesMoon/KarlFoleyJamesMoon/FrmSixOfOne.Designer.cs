a88d8e4b9b3e0d6365028b53975e0b17e36481ea
Karl Foley <c3274832@uon.edu.au>
2020-05-19 11:53:46 +1000

    Dice Rolling, working correctly

--- KarlFoleyJamesMoon/KarlFoleyJamesMoon/FrmSixOfOne.Designer.cs
+++ KarlFoleyJamesMoon/KarlFoleyJamesMoon/FrmSixOfOne.Designer.cs
@@ -34,24 +34,32 @@
             this.LblScorePlayerOne = new System.Windows.Forms.Label();
             this.LblScorePlayerTwo = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radButDiceNumberSix = new System.Windows.Forms.RadioButton();
+            this.radButDiceNumberFive = new System.Windows.Forms.RadioButton();
+            this.radButDiceNumberFour = new System.Windows.Forms.RadioButton();
+            this.radButDiceNumberThree = new System.Windows.Forms.RadioButton();
+            this.radButDiceNumberTwo = new System.Windows.Forms.RadioButton();
+            this.radButDiceNumberOne = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictBoxDiceOne = new System.Windows.Forms.PictureBox();
+            this.pictBoxDiceTwo = new System.Windows.Forms.PictureBox();
+            this.pictBoxDiceThree = new System.Windows.Forms.PictureBox();
+            this.pictBoxDiceFour = new System.Windows.Forms.PictureBox();
+            this.pictBoxDiceFive = new System.Windows.Forms.PictureBox();
+            this.pictBoxDiceSix = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceThree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceFour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceFive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceSix)).BeginInit();
             this.SuspendLayout();
             // 
             // LblPlayerOneName
             // 
             this.LblPlayerOneName.AutoSize = true;
-            this.LblPlayerOneName.Location = new System.Drawing.Point(12, 9);
+            this.LblPlayerOneName.Location = new System.Drawing.Point(207, 9);
             this.LblPlayerOneName.Name = "LblPlayerOneName";
             this.LblPlayerOneName.Size = new System.Drawing.Size(59, 13);
             this.LblPlayerOneName.TabIndex = 0;
@@ -60,7 +68,7 @@
             // LblPlayerTwoName
             // 
             this.LblPlayerTwoName.AutoSize = true;
-            this.LblPlayerTwoName.Location = new System.Drawing.Point(560, 9);
+            this.LblPlayerTwoName.Location = new System.Drawing.Point(653, 9);
             this.LblPlayerTwoName.Name = "LblPlayerTwoName";
             this.LblPlayerTwoName.Size = new System.Drawing.Size(60, 13);
             this.LblPlayerTwoName.TabIndex = 1;
@@ -68,17 +76,18 @@
             // 
             // BtnRoll
             // 
-            this.BtnRoll.Location = new System.Drawing.Point(354, 120);
+            this.BtnRoll.Location = new System.Drawing.Point(454, 120);
             this.BtnRoll.Name = "BtnRoll";
             this.BtnRoll.Size = new System.Drawing.Size(75, 23);
             this.BtnRoll.TabIndex = 2;
             this.BtnRoll.Text = "Roll !";
             this.BtnRoll.UseVisualStyleBackColor = true;
+            this.BtnRoll.Click += new System.EventHandler(this.BtnRoll_Click);
             // 
             // LblScorePlayerOne
             // 
             this.LblScorePlayerOne.AutoSize = true;
-            this.LblScorePlayerOne.Location = new System.Drawing.Point(77, 9);
+            this.LblScorePlayerOne.Location = new System.Drawing.Point(272, 9);
             this.LblScorePlayerOne.Name = "LblScorePlayerOne";
             this.LblScorePlayerOne.Size = new System.Drawing.Size(50, 13);
             this.LblScorePlayerOne.TabIndex = 3;
@@ -87,7 +96,7 @@
             // LblScorePlayerTwo
             // 
             this.LblScorePlayerTwo.AutoSize = true;
-            this.LblScorePlayerTwo.Location = new System.Drawing.Point(626, 9);
+            this.LblScorePlayerTwo.Location = new System.Drawing.Point(719, 9);
             this.LblScorePlayerTwo.Name = "LblScorePlayerTwo";
             this.LblScorePlayerTwo.Size = new System.Drawing.Size(50, 13);
             this.LblScorePlayerTwo.TabIndex = 4;
@@ -96,14 +105,93 @@
             // groupBox1
             // 
             this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox1.Controls.Add(this.radButDiceNumberSix);
+            this.groupBox1.Controls.Add(this.radButDiceNumberFive);
+            this.groupBox1.Controls.Add(this.radButDiceNumberFour);
+            this.groupBox1.Controls.Add(this.radButDiceNumberThree);
+            this.groupBox1.Controls.Add(this.radButDiceNumberTwo);
+            this.groupBox1.Controls.Add(this.radButDiceNumberOne);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(129, 42);
+            this.groupBox1.Location = new System.Drawing.Point(210, 42);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(563, 72);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
+            // 
+            // radButDiceNumberSix
+            // 
+            this.radButDiceNumberSix.AutoSize = true;
+            this.radButDiceNumberSix.Location = new System.Drawing.Point(470, 31);
+            this.radButDiceNumberSix.Name = "radButDiceNumberSix";
+            this.radButDiceNumberSix.Size = new System.Drawing.Size(42, 29);
+            this.radButDiceNumberSix.TabIndex = 12;
+            this.radButDiceNumberSix.TabStop = true;
+            this.radButDiceNumberSix.Text = "6";
+            this.radButDiceNumberSix.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.radButDiceNumberSix.UseVisualStyleBackColor = true;
+            // 
+            // radButDiceNumberFive
+            // 
+            this.radButDiceNumberFive.AutoSize = true;
+            this.radButDiceNumberFive.Location = new System.Drawing.Point(376, 31);
+            this.radButDiceNumberFive.Name = "radButDiceNumberFive";
+            this.radButDiceNumberFive.Size = new System.Drawing.Size(42, 29);
+            this.radButDiceNumberFive.TabIndex = 11;
+            this.radButDiceNumberFive.TabStop = true;
+            this.radButDiceNumberFive.Text = "5";
+            this.radButDiceNumberFive.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.radButDiceNumberFive.UseVisualStyleBackColor = true;
+            // 
+            // radButDiceNumberFour
+            // 
+            this.radButDiceNumberFour.AutoSize = true;
+            this.radButDiceNumberFour.Location = new System.Drawing.Point(277, 31);
+            this.radButDiceNumberFour.Name = "radButDiceNumberFour";
+            this.radButDiceNumberFour.Size = new System.Drawing.Size(42, 29);
+            this.radButDiceNumberFour.TabIndex = 10;
+            this.radButDiceNumberFour.TabStop = true;
+            this.radButDiceNumberFour.Text = "4";
+            this.radButDiceNumberFour.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.radButDiceNumberFour.UseVisualStyleBackColor = true;
+            // 
+            // radButDiceNumberThree
+            // 
+            this.radButDiceNumberThree.AutoSize = true;
+            this.radButDiceNumberThree.Location = new System.Drawing.Point(186, 31);
+            this.radButDiceNumberThree.Name = "radButDiceNumberThree";
+            this.radButDiceNumberThree.Size = new System.Drawing.Size(42, 29);
+            this.radButDiceNumberThree.TabIndex = 9;
+            this.radButDiceNumberThree.TabStop = true;
+            this.radButDiceNumberThree.Text = "3";
+            this.radButDiceNumberThree.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.radButDiceNumberThree.UseVisualStyleBackColor = true;
+            // 
+            // radButDiceNumberTwo
+            // 
+            this.radButDiceNumberTwo.AutoSize = true;
+            this.radButDiceNumberTwo.Location = new System.Drawing.Point(112, 31);
+            this.radButDiceNumberTwo.Name = "radButDiceNumberTwo";
+            this.radButDiceNumberTwo.Size = new System.Drawing.Size(42, 29);
+            this.radButDiceNumberTwo.TabIndex = 8;
+            this.radButDiceNumberTwo.TabStop = true;
+            this.radButDiceNumberTwo.Text = "2";
+            this.radButDiceNumberTwo.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.radButDiceNumberTwo.UseVisualStyleBackColor = true;
+            // 
+            // radButDiceNumberOne
+            // 
+            this.radButDiceNumberOne.AutoSize = true;
+            this.radButDiceNumberOne.Checked = true;
+            this.radButDiceNumberOne.Location = new System.Drawing.Point(30, 31);
+            this.radButDiceNumberOne.Name = "radButDiceNumberOne";
+            this.radButDiceNumberOne.Size = new System.Drawing.Size(42, 29);
+            this.radButDiceNumberOne.TabIndex = 7;
+            this.radButDiceNumberOne.TabStop = true;
+            this.radButDiceNumberOne.Text = "1";
+            this.radButDiceNumberOne.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.radButDiceNumberOne.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -115,56 +203,77 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "How many dice do you want to Roll";
             // 
-            // pictureBox1
+            // pictBoxDiceOne
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(70, 205);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.pictBoxDiceOne.BackColor = System.Drawing.Color.Black;
+            this.pictBoxDiceOne.Location = new System.Drawing.Point(374, 205);
+            this.pictBoxDiceOne.Name = "pictBoxDiceOne";
+            this.pictBoxDiceOne.Size = new System.Drawing.Size(100, 100);
+            this.pictBoxDiceOne.TabIndex = 6;
+            this.pictBoxDiceOne.TabStop = false;
+            this.pictBoxDiceOne.Visible = false;
             // 
-            // pictureBox2
+            // pictBoxDiceTwo
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(219, 205);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
+            this.pictBoxDiceTwo.BackColor = System.Drawing.Color.Black;
+            this.pictBoxDiceTwo.Location = new System.Drawing.Point(554, 205);
+            this.pictBoxDiceTwo.Name = "pictBoxDiceTwo";
+            this.pictBoxDiceTwo.Size = new System.Drawing.Size(100, 100);
+            this.pictBoxDiceTwo.TabIndex = 7;
+            this.pictBoxDiceTwo.TabStop = false;
+            this.pictBoxDiceTwo.Visible = false;
             // 
-            // pictureBox3
+            // pictBoxDiceThree
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(368, 205);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox3.TabIndex = 8;
-            this.pictureBox3.TabStop = false;
+            this.pictBoxDiceThree.BackColor = System.Drawing.Color.Black;
+            this.pictBoxDiceThree.Location = new System.Drawing.Point(200, 205);
+            this.pictBoxDiceThree.Name = "pictBoxDiceThree";
+            this.pictBoxDiceThree.Size = new System.Drawing.Size(100, 100);
+            this.pictBoxDiceThree.TabIndex = 8;
+            this.pictBoxDiceThree.TabStop = false;
+            this.pictBoxDiceThree.Visible = false;
             // 
-            // pictureBox4
+            // pictBoxDiceFour
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(503, 205);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox4.TabIndex = 9;
-            this.pictureBox4.TabStop = false;
+            this.pictBoxDiceFour.BackColor = System.Drawing.Color.Black;
+            this.pictBoxDiceFour.Location = new System.Drawing.Point(730, 205);
+            this.pictBoxDiceFour.Name = "pictBoxDiceFour";
+            this.pictBoxDiceFour.Size = new System.Drawing.Size(100, 100);
+            this.pictBoxDiceFour.TabIndex = 9;
+            this.pictBoxDiceFour.TabStop = false;
+            this.pictBoxDiceFour.Visible = false;
             // 
-            // pictureBox5
+            // pictBoxDiceFive
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(699, 205);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox5.TabIndex = 10;
-            this.pictureBox5.TabStop = false;
+            this.pictBoxDiceFive.BackColor = System.Drawing.Color.Black;
+            this.pictBoxDiceFive.Location = new System.Drawing.Point(27, 205);
+            this.pictBoxDiceFive.Name = "pictBoxDiceFive";
+            this.pictBoxDiceFive.Size = new System.Drawing.Size(100, 100);
+            this.pictBoxDiceFive.TabIndex = 10;
+            this.pictBoxDiceFive.TabStop = false;
+            this.pictBoxDiceFive.Visible = false;
+            // 
+            // pictBoxDiceSix
+            // 
+            this.pictBoxDiceSix.BackColor = System.Drawing.Color.Black;
+            this.pictBoxDiceSix.Location = new System.Drawing.Point(910, 205);
+            this.pictBoxDiceSix.Name = "pictBoxDiceSix";
+            this.pictBoxDiceSix.Size = new System.Drawing.Size(100, 100);
+            this.pictBoxDiceSix.TabIndex = 11;
+            this.pictBoxDiceSix.TabStop = false;
+            this.pictBoxDiceSix.Visible = false;
             // 
             // FrmSixOfOne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 450);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1046, 482);
+            this.Controls.Add(this.pictBoxDiceSix);
+            this.Controls.Add(this.pictBoxDiceFive);
+            this.Controls.Add(this.pictBoxDiceFour);
+            this.Controls.Add(this.pictBoxDiceThree);
+            this.Controls.Add(this.pictBoxDiceTwo);
+            this.Controls.Add(this.pictBoxDiceOne);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.LblScorePlayerTwo);
             this.Controls.Add(this.LblScorePlayerOne);
@@ -175,11 +284,12 @@
             this.Text = "Six of One";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceThree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceFour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceFive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxDiceSix)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,10 +304,17 @@
         private System.Windows.Forms.Label LblScorePlayerTwo;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictBoxDiceOne;
+        private System.Windows.Forms.PictureBox pictBoxDiceTwo;
+        private System.Windows.Forms.PictureBox pictBoxDiceThree;
+        private System.Windows.Forms.PictureBox pictBoxDiceFour;
+        private System.Windows.Forms.PictureBox pictBoxDiceFive;
+        private System.Windows.Forms.PictureBox pictBoxDiceSix;
+        private System.Windows.Forms.RadioButton radButDiceNumberSix;
+        private System.Windows.Forms.RadioButton radButDiceNumberFive;
+        private System.Windows.Forms.RadioButton radButDiceNumberFour;
+        private System.Windows.Forms.RadioButton radButDiceNumberThree;
+        private System.Windows.Forms.RadioButton radButDiceNumberTwo;
+        private System.Windows.Forms.RadioButton radButDiceNumberOne;
     }
 }
--- KarlFoleyJamesMoon/KarlFoleyJamesMoon/FrmSixOfOne.cs
+++ KarlFoleyJamesMoon/KarlFoleyJamesMoon/FrmSixOfOne.cs
@@ -12,9 +12,208 @@ namespace KarlFoleyJamesMoon
 {
     public partial class FrmSixOfOne : Form
     {
+        private Graphics graDiceOne;
+        private Graphics graDiceTwo;
+        private Graphics graDiceThree;
+        private Graphics graDiceFour;
+        private Graphics graDiceFive;
+        private Graphics graDiceSix;
+        private Graphics[] arrayOfGraphics;
+        private PictureBox[] arrayOfPictureBoxs;
+        private const int iAmountOfDice = 6;
+        private Random randNumber;
+
         public FrmSixOfOne()
         {
             InitializeComponent();
+            graDiceOne = pictBoxDiceOne.CreateGraphics();
+            graDiceTwo = pictBoxDiceTwo.CreateGraphics();
+            graDiceThree = pictBoxDiceThree.CreateGraphics();
+            graDiceFour = pictBoxDiceFour.CreateGraphics();
+            graDiceFive = pictBoxDiceFive.CreateGraphics();
+            graDiceSix = pictBoxDiceSix.CreateGraphics();
+            arrayOfGraphics = new Graphics[iAmountOfDice] {graDiceOne, graDiceTwo, graDiceThree, graDiceFour, graDiceFive, graDiceSix};
+            arrayOfPictureBoxs = new PictureBox[iAmountOfDice] {pictBoxDiceOne, pictBoxDiceTwo, pictBoxDiceThree, pictBoxDiceFour, pictBoxDiceFive, pictBoxDiceSix };
+            randNumber = new Random();
         }
+
+        private void BtnRoll_Click(object sender, EventArgs e)
+        {
+            Clear_Dice();
+            int iamountofDiceRolled = HowManyDiceRolled();
+            for(int iDiceNumber = 0; iDiceNumber < iamountofDiceRolled; iDiceNumber++)
+            {
+                int iDiceRoll = RollADice();
+                arrayOfPictureBoxs[iDiceNumber].Visible = true;
+                arrayOfPictureBoxs[iDiceNumber].Refresh();
+                arrayOfGraphics[iDiceNumber].Clear(Color.Black);
+                switch (iDiceRoll)
+                {
+                    case 1:
+                        diceSide_One(arrayOfGraphics[iDiceNumber]);
+                         break;
+                    case 2:
+                        diceSide_Two(arrayOfGraphics[iDiceNumber]);
+                        break;
+                    case 3:
+                        diceSide_Three(arrayOfGraphics[iDiceNumber]);
+                        break;
+                    case 4:
+                        diceSide_Four(arrayOfGraphics[iDiceNumber]);
+                        break;
+                    case 5:
+                        diceSide_Five(arrayOfGraphics[iDiceNumber]);
+                        break;
+                    case 6:
+                        diceSide_Six(arrayOfGraphics[iDiceNumber]);
+                        break;
+                    default:
+                        TextBox(iDiceNumber);
+                        break;
+                }
+            }
+
+
+        }
+
+        private void Clear_Dice()
+        {
+            for (int iDiceNumber = 0; iDiceNumber < iAmountOfDice; iDiceNumber++)
+            {
+                arrayOfGraphics[iDiceNumber].Clear(Color.Black);
+                //arrayOfPictureBoxs[iDiceNumber].Visible = false;
+                //pictBoxDiceOne.Visible = false;
+            }
+
+        }
+
+
+
+        private int HowManyDiceRolled()
+        {
+            if (radButDiceNumberOne.Checked) {return 1;}
+            else if (radButDiceNumberTwo.Checked) {return 2;}
+            else if (radButDiceNumberThree.Checked) {return 3;}
+            else if (radButDiceNumberFour.Checked) {return 4;}
+            else if (radButDiceNumberFive.Checked) {return 5;}
+            else {return 6;}
+        }
+
+        private int RollADice()
+        {
+            return randNumber.Next(1, 7);
+        }
+
+
+        //All below methords handle creating dice faces
+
+        private void diceSide_One(Graphics graDiceLocationNumber)
+        {
+            diceSide_CenterDot(graDiceLocationNumber);
+        }
+
+        private void diceSide_Two(Graphics graDiceLocationNumber)
+        {
+            diceSide_TopLeftDot(graDiceLocationNumber);
+            diceSide_BottomRight(graDiceLocationNumber);
+        }
+
+        private void diceSide_Three(Graphics graDiceLocationNumber)
+        {
+            diceSide_TopLeftDot(graDiceLocationNumber);
+            diceSide_CenterDot(graDiceLocationNumber);
+            diceSide_BottomRight(graDiceLocationNumber);
+        }
+
+        private void diceSide_Four(Graphics graDiceLocationNumber)
+        {
+            diceSide_TopLeftDot(graDiceLocationNumber);
+            diceSide_BottomRight(graDiceLocationNumber);
+            diceSide_TopRight(graDiceLocationNumber);
+            diceSide_BottomLeft(graDiceLocationNumber);
+
+        }
+
+        private void diceSide_Five(Graphics graDiceLocationNumber)
+        {
+            diceSide_CenterDot(graDiceLocationNumber);
+            diceSide_TopLeftDot(graDiceLocationNumber);
+            diceSide_BottomRight(graDiceLocationNumber);
+            diceSide_TopRight(graDiceLocationNumber);
+            diceSide_BottomLeft(graDiceLocationNumber);
+        }
+
+        private void diceSide_Six(Graphics graDiceLocationNumber)
+        {
+            diceSide_TopLeftDot(graDiceLocationNumber);
+            diceSide_BottomRight(graDiceLocationNumber);
+            diceSide_TopRight(graDiceLocationNumber);
+            diceSide_BottomLeft(graDiceLocationNumber);
+            diceSide_RightCenter(graDiceLocationNumber);
+            diceSide_LeftCenter(graDiceLocationNumber);
+        }
+
+        private void diceSide_CenterDot(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 45, 45, 10, 10);
+        }
+
+        private void diceSide_TopLeftDot(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 20, 20, 10, 10);
+        }
+
+        private void diceSide_BottomRight(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 70, 70, 10, 10);
+        }
+
+        private void diceSide_TopRight(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 70, 20, 10, 10);
+        }
+
+        private void diceSide_BottomLeft(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 20, 70, 10, 10);
+        }
+
+        private void diceSide_RightCenter(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 20, 45, 10, 10);
+        }
+
+        private void diceSide_LeftCenter(Graphics graDiceLocationNumber)
+        {
+            graDiceLocationNumber.FillEllipse(Brushes.White, 70, 45, 10, 10);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
