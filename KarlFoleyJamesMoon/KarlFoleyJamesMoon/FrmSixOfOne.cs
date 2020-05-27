cfa160990abb3d86e0687982a5e1239aaa3554ce
Karl Foley <c3274832@uon.edu.au>
2020-05-27 13:17:53 +1000

    Testing parameters set up on line 66

--- KarlFoleyJamesMoon/KarlFoleyJamesMoon/FrmSixOfOne.cs
+++ KarlFoleyJamesMoon/KarlFoleyJamesMoon/FrmSixOfOne.cs
@@ -63,6 +63,7 @@ namespace KarlFoleyJamesMoon
                     iScoreOnDice[iDiceNumber] = iDiceRoll;
                 }
             }
+            //iScoreOnDice = new int[] { 1, 1, 1, 1, 1, 1 };
             sCurrentSession.gCurrentGame.CountScore(iScoreOnDice);
             sCurrentSession.gCurrentGame.SwitchPlayer();
             lblPlayerTurn.Text = "Its " + sCurrentSession.gCurrentGame.Players[sCurrentSession.gCurrentGame.PlayerTurn].name + " Turn";
