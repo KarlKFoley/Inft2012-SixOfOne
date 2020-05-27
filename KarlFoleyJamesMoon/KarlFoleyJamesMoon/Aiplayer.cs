2d127449ae5a63e6c9c6e0d0eae7bf1e1b0669dd
James <james.moon@uon.edu.au>
2020-05-27 23:40:22 +1000

    Auto-determine dice roll
    Ripped my hair out for 4 hours, and gave up

--- KarlFoleyJamesMoon/KarlFoleyJamesMoon/Aiplayer.cs
+++ KarlFoleyJamesMoon/KarlFoleyJamesMoon/Aiplayer.cs
@@ -8,11 +8,56 @@ namespace KarlFoleyJamesMoon
 {
     class Aiplayer : Player
     {
-        Aiplayer() { }
+		private Player[] pPlayers = new Player[2];
+		private Random rnd;
+		Aiplayer() { }
 
         public String AiTurn(Game CurrentGame)
         {
-
+			return "";
         }
+
+		public int CheckPlayerScore()
+		{
+			int roll = 0;
+			/* if (pPlayers[1].Score == 0) {
+				roll = 4;
+			}
+			if (pPlayers[1].Score <= 100 % pPlayers[2].Score) {
+				if (pPlayers[1].Score < 50 % pPlayers[2].Score) {
+					roll = 4;
+				}
+				else 
+				{
+					roll = 5;
+				}
+			}
+			if (pPlayers[1].Score > 50 % pPlayers[2].Score) {
+				if (pPlayers[1].Score > 100 % pPlayers[2].Score) {
+					if (pPlayers[1].Score >= 500 % pPlayers[2].Score) {
+						roll = 1;
+					}
+					else 
+					{
+						roll = 2;
+					}
+				}
+				else {
+					roll = 3;
+				}
+			}
+			if (pPlayers[1].Score > 50 % score) {
+				roll = roll + 1;
+			}
+			return (roll); */
+			if (pPlayers[1].Score >= pPlayers[2].Score)
+			{
+				roll = rnd.Next(4,7);
+			}
+			if (pPlayers[1].Score < pPlayers[2].Score) {
+				roll = rnd.Next(2,4);
+			}
+			return roll;
+		}
     }
 }
